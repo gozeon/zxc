794ceaac14fb7e8f1294231294da3b3ae34d9952
gozeon <goze.qiu@gmail.com>
2023-11-16 11:37:04 +0800

    chore: format html

--- app/views.py
+++ app/views.py
@@ -26,4 +26,25 @@ def detail(request, app_id):
         page = get_object_or_404(Page, pk=page_id)
         context["page"] = page
 
+        from bs4 import BeautifulSoup
+        soup = BeautifulSoup(page.content, 'html.parser')
+
+        links = soup.findAll('link')
+        for link in links:
+            link.extract()
+
+        context['links'] = ''.join(str(l) for l in links)
+        print(links)
+        styles = soup.findAll('style')
+        for style in styles:
+            style.extract()
+        context['styles'] = ''.join(str(l) for l in styles)
+
+        scripts = soup.findAll('script')
+        for script in scripts:
+            script.extract()
+        context['scripts'] = ''.join(str(l) for l in scripts)
+
+        context['elements'] = str(soup)
+
     return render(request, "app/detail.html", context)
